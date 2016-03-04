119e84e504729ac57692b784ae106805288e24cb
北村裕介 <yk08072002@yahoo.co.jp>
2016-03-04 15:11:18 +0800

    user.php, users_controller.php新規作成

---
+++ controllers/users_controller.php
@@ -0,0 +1,60 @@
+<?php
+
+	require('models/user.php');
+
+	$controller = new UsersController();
+	//アクションによって呼び出すメソッドを変える
+	//$actionはroutes.phpで定義されているもの
+	switch ($action) {
+		case 'add':
+			$controller->add();
+			break;
+		case 'create':
+			$controller->create();
+			break;
+		case 'confirm':
+			$controller->confirm();
+			break;
+		default:
+			break;
+	}
+
+	//$controller->index();
+
+	class UsersController {
+		private $action = '';
+		private $resource = '';
+		private $viewOptions = '';
+
+		public function add() {
+			$this->action = 'add';
+
+			//ビューを呼び出す
+			include('views/layout/application.php');
+		}
+
+		public function create() {
+			//ここでモデルを呼び出す
+			$user = new User();
+			$this->viewOptions = $user->create();
+
+			$this->action = 'create';
+
+			//ビューを呼び出す
+			include('views/layout/application.php');
+		}
+
+		public function confirm() {
+			//ここでモデルを呼び出す
+			$user = new User();
+			$this->viewOptions = $user->confirm();
+
+			$this->action = 'confirm';
+
+			//ビューを呼び出す
+			include('views/layout/application.php');
+		}
+
+	}
+
+ ?>
