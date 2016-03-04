5156d7d3ddd6ee9e88344244279a97c8dfcded88
Kouhei Nishimura <koh.oct.un@gmail.com>
2016-03-04 15:09:20 +0800

    index.php, init commit

---
+++ views/contents/index.php
@@ -0,0 +1,125 @@
+<div id="preloader"></div>
+<div id="top"></div>
+
+<!-- /.parallax full screen background image -->
+<div class="fullscreen landing parallax" style="background-image:url('../asset/images/top_image.jpg');" data-img-width="2000" data-img-height="1333" data-diff="100">
+
+	<div class="overlay">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-7">
+					<!-- /.main title -->
+						<h1 class="wow fadeInLeft">
+						NexSeed Portal Site<br>New Open!!
+						</h1>
+
+					<!-- /.header paragraph -->
+					<div class="landing-text wow fadeInUp">
+						<p>留学生活を有意義にするために、是非このサイトをご活用下さい。<br>
+						皆さんが作り上げていくポータルサイトです。<br>
+						使い方は自由自在！！<br>
+						おすすめレストラン、ローカル情報、レジャー情報<br>
+						特に日常生活に関連する情報満載です！<br>
+						よろしくお願いします！</p>
+					</div>
+
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+
+
+
+<!-- /.intro section -->
+<div id="intro">
+	<div class="container">
+		<div class="row">
+			<!-- /.intro content -->
+			<form method="get" action="" role="form" name="search">
+				<div class="new-post col-md-6 wow slideInRight">
+					<h2>Let's Post Your Idea!</h2>
+					<div class="download-cta wow fadeInLeft">
+					<a class="btn-secondary" href="/nexseedportal/contents/add">New Post</a>
+					</div>
+				</div>
+				<div class="search col-md-6 wow slideInRight">
+					<h2>Search Information</h2>
+					<div class="btn-section dropdown fadeInLeft">
+						<select class="category" name="category">
+							<option value="1">Category</option>
+							<option value="2">レストラン</option>
+							<option value="3">スーパー</option>
+							<option value="4">商業施設</option>
+							<option value="5">レジャー</option>
+						</select>
+						<input type="text" name="search" placeholder="Search Word" class="form-control input-lg" value="">
+						<a href="#1st"><input type="button" class="btn-default" value="Search"></a>
+					</div>
+				</div>
+			</form>
+		</div>
+	</div>
+</div>
+
+<!-- /.feature section -->
+<div id="feature">
+	<div class="container">
+		<div class="text-center">
+			<h2 id="1st" class="wow fadeInLeft">Latest Posts</h2>
+			<div class="title-line wow fadeInRight"></div>
+		</div>
+		<div class="row">
+			<div class="col-sm-10 col-sm-offset-1">
+			   <div id="owl-testi" class="owl-carousel owl-theme wow fadeInUp">
+					<!-- /.testimonial 1 -->
+					<div class="testi-item">
+						<div class="box">
+							<table class="list">
+								<thead>
+									<tr>
+										<th>Shop</th>
+										<th>Review</th>
+										<th>Comment</th>
+									</tr>
+								</thead>
+								<tbody class="list-body text-overflow">
+									<tr data-href="/nexseedportal/contents/show/{id}">
+										<td data-label="shop">Bo's coffee</td>
+										<td class="review" data-label="review">★★★</td>
+										<td data-label="comment">コーヒーは美味しいけどwifiは普通だと思うんだけどどちらかといえばそうではないかもしれない</td>
+									</tr>
+									<!-- <tr data-href="show.html">
+										<td data-label="shop">STARBUCKS COFFEE</td>
+										<td class="review" data-label="review">★★★★★</td>
+										<td data-label="comment">コーヒーは美味しい、wifiは普通、サービスは最高だと思うんだけどどちらかといえばそうではないかもしれない</td>
+									</tr>
+									<tr data-href="show.html">
+										<td data-label="shop">coffee dream</td>
+										<td class="review" data-label="review">★★★★★</td>
+										<td data-label="comment">コーヒーは普通だけどwifiは爆速だと思うんだけどどちらかといえばそうではないかもしれない</td>
+									</tr>
+									<tr data-href="show.html">
+										<td data-label="shop">xxxxxxxx</td>
+										<td class="review" data-label="review">★</td>
+										<td data-label="comment">good</td>
+									</tr>
+									<tr data-href="show.html">
+										<td data-label="shop">xxxxxxxx</td>
+										<td class="review" data-label="review">★</td>
+										<td data-label="comment">good</td>
+									</tr> -->
+								</tbody>
+							</table>
+						</div>
+					</div>
+				</div>
+				<div class="btn-section">
+					<a href="" class="btn-default" style="float:right">&nbsp;&nbsp;&nbsp;&nbsp;Next&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a>
+					<a href="" class="btn-default" style="float:right">Previous</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
