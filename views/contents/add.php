1aab696292cb12537ca2d981bb765ca5bba42b37
茂木雄一郎 <mogi.3614@gmail.com>
2016-03-05 09:24:07 +0800

    map変更

--- views/contents/add.php
+++ views/contents/add.php
@@ -21,19 +21,58 @@
 							<input type="text" name="Store Name" placeholder="Store Name" class="form-control input-lg" value="">
 						</div>
 							<!-- 場所の写真 -->
-						<div class="category">
+						<!-- <div class="category">
 							<input type="text" name="Location" placeholder="Location" class="form-control input-lg" value="">
 							<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
     						<script type="text/javascript" src="js/jquery.js"></script>
    							<script type="text/javascript" src="js/map.js"></script>
    							<div id="map" class="map01"></div>
-						</div>
+						</div> -->
 						<!-- <div align="right" style="float: right">
 							<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
     						<script type="text/javascript" src="js/jquery.js"></script>
    							<script type="text/javascript" src="js/map.js"></script>
    							<div id="map" style="width:200px; height:200px"></div>
    						</div> -->
+   						 <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+					    <!-- googlemap API -->
+					    <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
+					    <!-- googlemap API 実装 -->
+					    <script type="text/javascript">
+					        google.maps.event.addDomListener(window, 'load', function()
+					        {
+					            // La Guardia Flats2の経緯度
+					            var lng = 123.90381932258606;
+					            var lat = 10.329200473939935;
+					            var latlng = new google.maps.LatLng(lat, lng);
+
+					            var mapOptions = {
+					                zoom: 15,
+					                center: latlng,
+					                mapTypeId: google.maps.MapTypeId.ROADMAP,
+					                scaleControl: true
+					            };
+					            // マップを表示する
+					            var mapObj = new google.maps.Map(document.getElementById('gmap'), mapOptions);
+					            // マップ上にマーカーを表示する
+					            var markerObj = new google.maps.Marker({
+					                position: latlng,
+					                map: mapObj
+					            });
+
+					            // クリック時のイベント
+					            google.maps.event.addListener(mapObj, 'click', function(e)
+					            {
+					                // ポジションを変更
+					                markerObj.position = e.latLng;
+					                // マーカーをセット
+					                markerObj.setMap(mapObj);
+					                alert("経度:" + e.latLng.lat() + "  緯度:" + e.latLng.lng());
+					            })
+					        });
+					    </script>
+					    <div id="gmap" class="map01"style=""></div>
+
 							<!-- 写真 -->
 						<div class="category">
 				           	<input type="file" name="Photo" placeholder="Photo" class="form-control input-lg" value="">
@@ -63,7 +102,7 @@
 						</div>
 							<!-- 確認ボタン -->
 						<div class="col-sm-4 contact-left">
-							<a href = "/NexSeedPortal/contents/index">
+							<a href = "/NexSeedPortal/contents/add_check">
 							<input type="submit" name="submit" value="Post" class="btn btn-success wow fadeInUp" />
 						</div>
 						 </form>
