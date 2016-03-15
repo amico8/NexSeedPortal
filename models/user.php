6f5549bacb19d8d0602bb39e24ab9a97533fe2b1
北村裕介 <yk08072002@yahoo.co.jp>
2016-03-15 16:06:59 +0800

    ヘッダーのログアウト部分実装

--- models/user.php
+++ models/user.php
@@ -11,91 +11,114 @@
 
 		public function login($post) {
 			if (isset($_COOKIE['email']) && $_COOKIE['email'] != '') {
-			    $post['email'] = $_COOKIE['email'];
-			    $post['password'] = $_COOKIE['password'];
-			    $post['save'] = 'on';
+				$post['email'] = $_COOKIE['email'];
+				$post['password'] = $_COOKIE['password'];
+				$post['save'] = 'on';
 			}
+
 			if (!empty($post)) {
-		    	if ($post['email'] != '' && $post['password'] != '') {
-			    	$sql = sprintf('SELECT COUNT(*) AS cnt FROM users WHERE email="%s"',
-			        	mysqli_real_escape_string($this->dbconnect, $post['email'])
-			    	);
-				    $record = mysqli_query($this->dbconnect, $sql);
-				    $table = mysqli_fetch_assoc($record);
-				    if ($table['cnt'] == 0) {
-				        $error['login'] = 'noexist';
-				    } elseif (strlen($post['password']) < 4 || strlen($post['password']) > 16) {
-				    	$error['login'] = 'length';
-				    } else {
-				    	$sql = sprintf('SELECT * FROM users WHERE email="%s" AND password="%s"',
-				        	mysqli_real_escape_string($this->dbconnect, $post['email']),
-				        	mysqli_real_escape_string($this->dbconnect, sha1($post['password']))
-			        	);
-			      		$record = mysqli_query($this->dbconnect, $sql) or die(mysqli_error($this->dbconnect));
-			      		if ($table = mysqli_fetch_assoc($record)) {
-				        	//ログイン成功
-				        	$_SESSION['user_id'] = $table['user_id'];
-				        	$_SESSION['time'] = time();
-				        	if ($post['save'] == 'on') {
-					          	setcookie('email', $post['email'], time()+60*60*24*14);
-					          	setcookie('password', $post['password'], time()+60*60*24*14);
-				        	}
-				        	$_SESSION['post'] = $post;
-				        	header('Location: /NexSeedPortal/contents/index');
-				        	exit();
-				      	} else {
-				        	$error['login'] = 'failed';
-				      	}
-				    }
-		    	} else {
-		      		$error['login'] = 'blank';
-		    	}
+				if ($post['email'] != '' && $post['password'] != '') {
+					$sql = sprintf('SELECT COUNT(*) AS cnt FROM users WHERE email="%s"',
+						mysqli_real_escape_string($this->dbconnect, $post['email'])
+					);
+					$record = mysqli_query($this->dbconnect, $sql);
+					$table = mysqli_fetch_assoc($record);
+					if ($table['cnt'] == 0) {
+						$error['login'] = 'noexist';
+					} elseif (strlen($post['password']) < 4 || strlen($post['password']) > 16) {
+						$error['login'] = 'length';
+					} else {
+						$sql = sprintf('SELECT * FROM users WHERE email="%s" AND password="%s"',
+							mysqli_real_escape_string($this->dbconnect, $post['email']),
+							mysqli_real_escape_string($this->dbconnect, sha1($post['password']))
+						);
+				  		$record = mysqli_query($this->dbconnect, $sql) or die(mysqli_error($this->dbconnect));
+						if ($table = mysqli_fetch_assoc($record)) {
+							//ログイン成功
+							$_SESSION['user_id'] = $table['user_id'];
+							$_SESSION['time'] = time();
+							if ($post['save'] == 'on' && !isset($_COOKIE)) {
+							  	setcookie('email', $post['email'], time()+60*60*24*14);
+							  	setcookie('password', $post['password'], time()+60*60*24*14);
+							}
+							$_SESSION['post'] = $post;
+							header('Location: /NexSeedPortal/contents/index');
+							exit();
+					  	} else {
+							$error['login'] = 'failed';
+					  	}
+					}
+				} else {
+			  		$error['login'] = 'blank';
+				}
 				$this->error = $error;
 			}
+		}
+
+		public function logout() {
+			//セッション情報を削除
+			$_SESSION = array();
+			if (ini_get('session.use_cookies')) {
+				$params = session_get_cookie_params();
+				var_dump($params);
+				setcookie(session_name(), '', time() - 42000,
+				$params['path'], $params['domain'],
+				$params['secure'], $params['httponly']
+				);
+		 	}
+			session_destroy();
+
+			//Cookie情報も削除
+			$_COOKIE = array();
+			setcookie('email', '', time()-42000);
+			setcookie('password', '', time()-42000);
+
+			header('Location: /NexSeedPortal/users/login/');
+			exit();
 		}
 
 		public function add($post) {
 			$error = array();
 			if ($post == '' && isset($_SESSION) && !empty($_SESSION)) {
-			    $this->rewrite = $_SESSION['join'];
-			    $error['rewrite'] = true;
+				$this->rewrite = $_SESSION['join'];
+				$error['rewrite'] = true;
 			}
 			if(isset($post) && !empty($post)) {
-			    $name = htmlspecialchars($post['name'], ENT_QUOTES, 'UTF-8');
-			    $email = htmlspecialchars($post['email'], ENT_QUOTES, 'UTF-8');
-			    $password = htmlspecialchars($post['password1'], ENT_QUOTES, 'UTF-8');
-			    if($post['name']=='') {
-			    	$error['name'] = 'blank';
-			    }
-			    if($post['email']=='') {
-			    	$error['email'] = 'blank';
-			    }
-			    if($post['password1']=='') {
-			     	$error['password'] = 'blank';
-			    } elseif (strlen($post['password1']) < 4 || strlen($post['password1']) > 16) {
-			    	$error['password'] = 'length';
-			    } elseif ($post['password1'] != $post['password2']) {
-			    	$error['password'] = 'incorrect';
-			    }
-			    if (empty($error)) {
-			    	//重複アカウントのチェック
-			    	$sql = sprintf('SELECT COUNT(*) AS cnt FROM users WHERE email="%s"',
-			        	mysqli_real_escape_string($this->dbconnect, $post['email'])
-			    	);
-				    $record = mysqli_query($this->dbconnect, $sql);
-				    $table = mysqli_fetch_assoc($record);
-				    if ($table['cnt'] > 0) {
-				        $error['email'] = 'duplicate';
-				    } else {
-				        $_SESSION['join'] = $post;
-				        header('Location: /NexSeedPortal/users/confirm/');
-				        exit();
-				    }
-			    }
+				$name = htmlspecialchars($post['name'], ENT_QUOTES, 'UTF-8');
+				$email = htmlspecialchars($post['email'], ENT_QUOTES, 'UTF-8');
+				$password = htmlspecialchars($post['password1'], ENT_QUOTES, 'UTF-8');
+				if($post['name']=='') {
+					$error['name'] = 'blank';
+				}
+				if($post['email']=='') {
+					$error['email'] = 'blank';
+				}
+				if($post['password1']=='') {
+				 	$error['password'] = 'blank';
+				} elseif (strlen($post['password1']) < 4 || strlen($post['password1']) > 16) {
+					$error['password'] = 'length';
+				} elseif ($post['password1'] != $post['password2']) {
+					$error['password'] = 'incorrect';
+				}
+				if (empty($error)) {
+					//重複アカウントのチェック
+					$sql = sprintf('SELECT COUNT(*) AS cnt FROM users WHERE email="%s"',
+						mysqli_real_escape_string($this->dbconnect, $post['email'])
+					);
+					$record = mysqli_query($this->dbconnect, $sql);
+					$table = mysqli_fetch_assoc($record);
+					if ($table['cnt'] > 0) {
+						$error['email'] = 'duplicate';
+					} else {
+						$_SESSION['join'] = $post;
+						header('Location: /NexSeedPortal/users/confirm/');
+						exit();
+					}
+				}
 			}
 			if (isset($_REQUEST['action']) && $_REQUEST['action']=='rewrite') {
-			    $this->rewrite = $_SESSION['join'];
-			    $error['rewrite'] = true;
+				$this->rewrite = $_SESSION['join'];
+				$error['rewrite'] = true;
 			}
 			$this->error = $error;
 		}
@@ -110,9 +133,9 @@
 		public function create() {
 			//登録処理
 			$sql = sprintf('INSERT INTO users SET user_name="%s", email="%s", password="%s", created=now()',
-			    mysqli_real_escape_string($this->dbconnect, $_SESSION['join']['name']),
-			    mysqli_real_escape_string($this->dbconnect, $_SESSION['join']['email']),
-			    mysqli_real_escape_string($this->dbconnect, sha1($_SESSION['join']['password1']))
+				mysqli_real_escape_string($this->dbconnect, $_SESSION['join']['name']),
+				mysqli_real_escape_string($this->dbconnect, $_SESSION['join']['email']),
+				mysqli_real_escape_string($this->dbconnect, sha1($_SESSION['join']['password1']))
 			);
 			mysqli_query($this->dbconnect, $sql) or die(mysqli_error($this->dbconnect));
 			//登録したので、セッション情報を破棄
--- views/layout/application.php
+++ views/layout/application.php
@@ -29,32 +29,29 @@
 </head>
 
 <body data-spy="scroll" data-target="#navbar-scroll">
-	<!-- さとしさん担当 -->
 <div id="menu">
 	<nav class="navbar-wrapper navbar-default" role="navigation">
 		<div class="container">
-			  <div class="navbar-header">
-				<!-- <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-backyard">
-				  <span class="sr-only">Toggle navigation</span>
-				  <span class="icon-bar"></span>
-				  <span class="icon-bar"></span>
-				  <span class="icon-bar"></span>
-				</button> -->
-				<a class="navbar-brand site-name" href="#"><img src="/NexSeedPortal/webroot/asset/images/nex.png" alt="logo"></a>
+			<div class="navbar-header">
+				<a class="navbar-brand site-name" href="/NexSeedPortal/contents/index/"><img src="/NexSeedPortal/webroot/asset/images/nex.png" alt="logo"></a>
 				<ul class="nav navbar-nav">
 				<li>
-					<div class="navbar-brand2">Welcome xxxxx！</div>
+					<div class="navbar-brand2">
+						<?php if (isset($_SESSION['user_id']) && $_SESSION['time'] + 3600 > time()) {
+							echo 'Welcome '.$_SESSION['user_name'];
+						} ?>
+					</div>
 				</li>
 				</ul>
-			  </div>
+			</div>
 	 		<!-- NAVIGATION -->
-			  <div id="navbar-scroll" class="collapse navbar-collapse navbar-backyard navbar-right">
+			<div id="navbar-scroll" class="collapse navbar-collapse navbar-backyard navbar-right">
 				<ul class="nav navbar-nav">
 				<li>
-					<div class="btn-section"><a href="join/index.html" button type="button" class="btn-default2">Logout</a></div>
+					<div class="btn-section"><a href="/NexSeedPortal/users/logout/" button type="button" class="btn-default2">Logout</a></div>
 				</li>
 				</ul>
-			  </div>
+			</div>
 		</div>
 	</nav>
 </div>
@@ -63,14 +60,7 @@
 	include('views/' . $this->resource. '/' . $this->action . '.php');
  ?>
 
-
-
-<!-- /.testimonial section -->
-<!-- <div id="testi">
-	<div class="container">
-	</div>
-</div>
- --><!-- /.footer -->
+<!-- /.footer -->
 <footer id="footer">
 	<div class="container">
 		<div class="col-sm-4 col-sm-offset-4">
@@ -80,13 +70,13 @@
 						<li><a class="wow fadeInUp" href="https://twitter.com/NexSeed_Cebu?lang=ja" target="_blank"><i class="fa fa-twitter"></i></a></li>
 						<li><a class="wow fadeInUp" href="https://www.facebook.com/NexSeed/?ref=br_rs" target="_blank" data-wow-delay="0.2s"><i class="fa fa-facebook"></i></a></li>
 					</ul>
-				</div>	
+				</div>
 			<div class="text-center wow fadeInUp" style="font-size: 14px;">Copyright NexSeed Portal since 2016</div>
 			<a href="#" class="scrollToTop"><i class="pe-7s-up-arrow pe-va"></i></a>
-		</div>	
-	</div>	
+		</div>
+	</div>
 </footer>
-	
+
 	<!-- /.javascript files -->
     <script src="/NexSeedPortal/webroot/asset/js/jquery.js"></script>
     <script src="/NexSeedPortal/webroot/asset/js/bootstrap.js"></script>
