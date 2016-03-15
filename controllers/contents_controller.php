4557492293bee344a1bf982ec29ae1849beaa134
amico <amico8amico@gmail.com>
2016-03-15 19:10:27 +0800

    編集→編集確認→更新部分のファイル操作をレビュー

--- controllers/contents_controller.php
+++ controllers/contents_controller.php
@@ -25,6 +25,11 @@
 			break;
 
 		case 'edit':
+			$sessionEdit = array();
+			if (isset($_SESSION['edit']) && !empty($_SESSION['edit'])) {
+				// 「back」ボタン押下時は、セッションから値を取得して表示する
+				$sessionEdit = $_SESSION['edit'];
+			}
 			$controller->edit($id, $sessionEdit);
 			break;
 
@@ -33,11 +38,16 @@
 			break;
 
 		case 'confirm':
-			$controller->editConfirm($id, $post, $fileName, $files, $sessionEdit);
+			// セッションに$_FILESのデータを格納
+			$_SESSION['edit'] = $post;
+			$_SESSION['edit'] += array('picture_name'=>$_FILES['picture_path']['name']);
+			$_SESSION['edit'] += array('picture_tmp'=>$_FILES['picture_path']['tmp_name']);
+			$_SESSION['edit'] += array('picture_path'=>date('YmdHis').$_FILES['picture_path']['name']);
+			$controller->editConfirm($id, $_SESSION['edit']);
 			break;
 
 		case 'update':
-			$controller->update($id, $sessionEdit);
+			$controller->update($id, $_SESSION['edit']);
 			break;
 
 		default:
@@ -52,7 +62,6 @@
 		private $categories = '';
 		private $files = '';
 		private $post = array();
-
 
 		public function show($id) {
 			// モデルを呼び出す
@@ -77,31 +86,27 @@
 			include('views/layout/application.php');
 		}
 
-		public function editConfirm($id, $post, $fileName, $files, $sessionEdit) {
-			if (!empty($fileName)) {
-		    	$ext = substr($fileName, -3);
-		    	if ($ext != 'jpg' && $ext != 'gif' && $ext != 'png' && $ext != 'JPG' && $ext != 'GIF' && $ext != 'PNG'){
-		      		$_SESSION['error'] = 'error_prefix';
-		      		header('Location: /NexSeedPortal/contents/edit/'. $id);
-		    	} else {
-		    		$_SESSION['error'] = 'select_again';
-		    	}
-		  	} else {
-		  		$_SESSION['error'] = 'no_error';
-		  	}
+		public function editConfirm($id, $sessionEdit) {
+			// 画像をアップロードする場合
+			if (!empty($sessionEdit['picture_name'])) {
+				// 拡張子チェック
+				$ext = substr($sessionEdit['picture_name'], -3);
+				if ($ext != 'jpg' && $ext != 'gif' && $ext != 'png' && $ext != 'JPG' && $ext != 'GIF' && $ext != 'PNG'){
+					$_SESSION['error'] = 'error_prefix';
+					header('Location: /NexSeedPortal/contents/edit/'. $id);
+				} else {
+					// 問題なければアップロード
+					move_uploaded_file(
+						$sessionEdit['picture_tmp'],
+						'webroot/asset/images/post_images/'. $sessionEdit['picture_path']);
+					$this->files = $sessionEdit['picture_path'];
+					$_SESSION['error'] = 'select_again';
+				}
+			} else {
+				$_SESSION['error'] = 'no_error';
+			}
 			$content = new Content();
 			$this->categories = $content->selectCategories();
-			$this->post = $post;
-			$this->files = $files;
-			//ここに書く！！画像アップロード！！
-			//エラーが出たら、エラーの値をreturn値で表示し、それをedit.phpに表示する
-			// if (isset($fileName)) {
-			// 	$picture_path = date('YmdHis') . $fileName;
-			// 	move_uploaded_file($_FILES['picture_path']['tmp_name'], 'webroot/asset/images/post_images/'. $picture_path);
-			// 	$files = $picture_path;
-			// 	$this->files = $files;
-			// }
-
 			$this->resource = 'contents';
 			$this->action = 'editConfirm';
 
@@ -109,13 +114,12 @@
 		}
 
 		public function update($id, $sessionEdit) {
-			var_dump($sessionEdit);
+			// var_dump($sessionEdit);
 			$content = new Content();
 			$content->update($id, $sessionEdit);
 
 			header('Location: /NexSeedPortal/contents/show/'.$id);
 		}
-
 
 		public function delete($id) {
 			$content = new content();
@@ -123,10 +127,5 @@
 
 			header('Location: /NexSeedPortal/contents/index');
 		}
-
-		
-
-
 	}
-
  ?>
