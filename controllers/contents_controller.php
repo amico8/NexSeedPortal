253400ea016beba3c6f8e85b58a4e4fecd975780
amico <amico8amico@gmail.com>
2016-03-17 09:42:37 +0800

    controller整理

--- controllers/contents_controller.php
+++ controllers/contents_controller.php
@@ -14,16 +14,10 @@
     }
     //コントローラのクラスをインスタンス化
     $controller = new ContentsController();
-
-    //アクション名によって、呼び出すメソッドを変える
-    //$action (グローバル変数)は、routes.phpで定義されているもの
-
     switch ($action) {
         case 'index':
             $controller->index($id,$post);
             break;
-
-
 
         default:
             break;
@@ -31,25 +25,24 @@
 
     class ContentsController{
         //プロパティ
+        private $content = '';
         private $action = '';
         private $resource = '';
         private $viewOptions = '';
+        private $loginAction = '';
 
-        private $loginaction = '';
-
-
-        public function index($id,$post){
+        public function __construct(){
             //モデルを呼び出す
-                $content = new Content();
-                $this->viewOptions = $content->index($id,$post);
-            //アクション名を設定
-                $this->resource = 'contents';
-                $this->action = 'index';
-            //ビューを呼び出す
-                require('views/layout/application.php');
+            $this->content = new Content();
         }
 
-
+        public function index($id,$post){
+            $this->viewOptions = $this->content->index($id,$post);
+            //アクション名を設定
+            $this->resource = 'contents';
+            $this->action = 'index';
+            //ビューを呼び出す
+            require('views/layout/application.php');
+        }
     }
-
  ?>
