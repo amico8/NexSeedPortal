c672ca8ebffb9fae6d2a685d80a05df948f695d7
huro3h_satoshi <huro3h@gmail.com>
2016-03-17 15:51:01 +0800

    add インデント修正

--- controllers/contents_controller.php
+++ controllers/contents_controller.php
@@ -6,8 +6,8 @@
 
 	switch ($action) {
 		case 'index';
-		    $controller -> index($id,$post);
-		    break;
+			$controller -> index($id,$post);
+			break;
 
 		case 'add':
 			$controller->add();
@@ -20,13 +20,13 @@
 			} else {
 				$controller->editConfirm($id);
 			}
-            break;
-        // メソッドを呼び出す時のみスーパーグローバル変数を使う。
-        case 'create':
+			break;
+		// メソッドを呼び出す時のみスーパーグローバル変数を使う。
+		case 'create':
 			$controller->create();
 			unset($_SESSION['add']);
-            unset($_SESSION['error']);
-       		header('Location:/NexSeedPortal/contents/index');
+			unset($_SESSION['error']);
+	   		header('Location:/NexSeedPortal/contents/index');
 			break;
 
 		default:
@@ -42,15 +42,15 @@
 		private $files = '';
 
 		public function index($id,$post){
-    		//モデルを呼び出す
-        	$content = new Content();
-            $this->viewOptions = $content->index($id,$post);
-        	//アクション名を設定
-            $this->resource = 'contents';
-            $this->action = 'index';
-        	//ビューを呼び出す
-            require('views/layout/application.php');
-    	}
+			//モデルを呼び出す
+			$content = new Content();
+			$this->viewOptions = $content->index($id,$post);
+			//アクション名を設定
+			$this->resource = 'contents';
+			$this->action = 'index';
+			//ビューを呼び出す
+			require('views/layout/application.php');
+		}
 
 		public function add(){
  			$content = new Content();
@@ -61,10 +61,12 @@
 		}
 
 		public function addConfirm($files, $fileName) {
-		    if (isset($_FILES['picture_path']['name']) && !empty($_FILES['picture_path']['name'])) {
-		        $fileName = $_FILES['picture_path']['name'];
-		        $files = $_FILES['picture_path'];
+			if (isset($_FILES['picture_path']['name']) && !empty($_FILES['picture_path']['name'])) {
+				$fileName = $_FILES['picture_path']['name'];
+				$files = $_FILES['picture_path'];
 				if (!empty($fileName)) {
+
+					
 					$ext = substr($fileName, -3);
 					if ($ext != 'jpg' && $ext != 'gif' && $ext != 'png' && $ext != 'JPG' && $ext != 'GIF' && $ext != 'PNG'){
 						$_SESSION['error'] = 'error_prefix';
@@ -78,7 +80,7 @@
 				} else {
 					$_SESSION['error'] = 'no_error';
 				}
-	            $_SESSION['add'] += array('picture_path'=>$files);
+				$_SESSION['add'] += array('picture_path'=>$files);
 			}
 			$content = new Content();
 			$this->categories = $content->selectCategories();
@@ -87,15 +89,15 @@
 			$this->action = 'add_confirm';
 			include('views/layout/application.php');
 		}
-	    public function create() {
-		    $content = new Content();
+		public function create() {
+			$content = new Content();
 			$content->create();
-		    // include('views/layout/application.php');
+			// include('views/layout/application.php');
 		}
-	    public function editConfirm($id) {
-		    $this->resource = 'contents';
-		    $this->action = 'edit_confirm';
-		    include('views/layout/application.php');
+		public function editConfirm($id) {
+			$this->resource = 'contents';
+			$this->action = 'edit_confirm';
+			include('views/layout/application.php');
 
 		}
 
--- routes.php
+++ routes.php
@@ -1,25 +1,25 @@
 <?php
 
-    session_start();
-    //$GETパラメータ取得
-    //explode関数:第二引数の文字列を第一引数で分割し、配列で返す関数
-    $params = explode('/', $_GET['url']);
+	session_start();
+	//$GETパラメータ取得
+	//explode関数:第二引数の文字列を第一引数で分割し、配列で返す関数
+	$params = explode('/', $_GET['url']);
 
-    //パラメータの分解
-    $resource = $params[0];
-    $action = $params[1];
-    $id = 0;
-    $post = array();
-    $files = '';
-    $fileName = '';
+	//パラメータの分解
+	$resource = $params[0];
+	$action = $params[1];
+	$id = 0;
+	$post = array();
+	$files = '';
+	$fileName = '';
 
-    //idがあった場合idも取得する
-    if (isset($params[2])) {
-        $id = $params[2];
-    }
-    if (isset($_POST) && !empty($_POST)) {
-        $post = $_POST;
-    }
-    //コントローラの呼び出し
-    require('controllers/'.$resource.'_controller.php');
+	//idがあった場合idも取得する
+	if (isset($params[2])) {
+		$id = $params[2];
+	}
+	if (isset($_POST) && !empty($_POST)) {
+		$post = $_POST;
+	}
+	//コントローラの呼び出し
+	require('controllers/'.$resource.'_controller.php');
 ?>
