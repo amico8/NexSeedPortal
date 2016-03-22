21f97ce47ace7270dc3dcbdffd5e93c8ba3709c7
Kouhei Nishimura <koh.oct.un@gmail.com>
2016-03-22 14:53:50 +0800

    不要なlogin()を消去

--- controllers/function.php
+++ controllers/function.php
@@ -1,32 +1,4 @@
 <?php
-//最初にログインするときにログイン処理を行う関数
-function login($post,$db){
-	$error = array();
-	if (isset($post)&&!empty($post)) {
-		if ($post['email'] !='' && $post['password'] != '') {
-			$sql = sprintf('SELECT * FROM `users` WHERE `email` = "%s" AND `password` = "%s"',
-				mysqli_real_escape_string($db, $post['email']),
-				mysqli_real_escape_string($db, sha1($post['password'])));
-			$record = mysqli_query($db,$sql) or die(mysqli_error($db));
-			if ($table = mysqli_fetch_assoc($record)) {
-				$_SESSION['user_id'] = $table['user_id'];
-				$_SESSION['user_name'] = $table['user_name'];
-				$_SESSION['time'] = time();
-				if ($_POST['save'] = 'on') {
-					setcookie('email',$post['email'],time() +60*60*24*14);
-					setcookie('password',$post['password'],time() +60*60*24*14);
-				}
-			}else{
-				$error['login'] = 'failed';
-				header('Location: /NexSeedPortal/users/login/');
-			}
-		}else {
-			$error['login'] = 'blank';
-			header('Location: /NexSeedPortal/users/login/');
-		}
-	}
-}
-
 //ログイン後にページ遷移したときにログインチェックを行う関数
 function loginCheck($session,$db){
 	if (isset($session['user_id']) && $session['time'] + 3600 > time()) {
